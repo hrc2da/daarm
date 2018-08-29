0ebce61cd8254c75421caeb8dbf5936d91dd39e9
mvl24 <mvl24@cornell.edu>
2018-08-29 17:07:23 -0400

    added simple LR model to agent

--- src/EOSSModel.py
+++ src/EOSSModel.py
@@ -2,16 +2,42 @@ from daarm.srv import *
 import rospy
 import csv
 
-
-from sklearn import linear_model.LinearRegression as LR
+import numpy as np
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.model_selection import train_test_split
 
 class EOSSModel:
-    def __init__(self):
+
+    def __init__(self,predata_path=""):
         self.model = LR()
         self.configs = {}
-        self.init_evaluate_server()
+        self.batch_size = 1
+        self.configs['0' * 60] = (0,0)
+        if(predata_path):
+            self.load_data(predata_path)
+        self.train_model()
+        self.initevaluate_server()
+        rospy.Subscriber("/configs", String, self.add_training_point)
+        
+    def add_training_point(self, ros_msg):
+        point = json.loads(message.data)
+        self.configs[point['config']] = (point['science'], point['cost'])
+        if len(self.configs) % self.batch_size == 0:
+            self.train_model()
+
     def handle_evaluate_config(self,req):
-        return this.model.evaluate(req.config)
+        return this.model.predict(self.conv_bit_list(req.config))
+
+    @staticmethod
+    def conv_bit_list(bitstring):
+        return [int(bit) for bit in bitstring]
+
+    def train_model(self):
+        X = np.array([self.conv_bit_list(config) for config in self.configs.keys()])
+        Y = np.array(self.configs.values())
+
+        xTr, xTe, yTr, yTe = train_test_split(X, Y, shuffle = True)
+        self.model.fit(xTr,yTr)
 
     def init_evaluate_server(self):
         rospy.init_node('EOSS_model_server')
@@ -19,7 +45,17 @@ class EOSSModel:
         rospy.spin()
 
     def load_data(self,path):
-        return None
-subscribe and update the model on new evals
+        try:
+            with open(path,'rb') as predata:
+                csvreader = csv.reader(predata,delimiter = ',')
+                next(csvreader)
+                for row in csvreader:
+                    self.configs[row[0]] = (float(row[1]),float(row[2])) #config = science,cost
+        except IOError as e:
+            print(e)
+            return
 
+
+if __name__ == '__main__':
+    m = EOSSModel("./model/raw_combined_data.csv")
 
